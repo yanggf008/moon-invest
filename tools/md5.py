827c8e2849712636ff751cc09851a855dc829219
yanggf008 <yanggf008@126.com>
2019-04-10 11:07:27 +1200

    change md5 into object-oriented

--- tools/md5.py
+++ tools/md5.py
@@ -3,15 +3,16 @@ import hashlib, urllib
 
 
 # using built-in hashlib package get md5 digest of your OKEX key
-
-def get_sign_value(dataMap):
-    dataMaps = sorted(dataMap.items(), key=lambda e: e[0], reverse=False)
-    str = urllib.urlencode(dataMaps)
-    secret_key = "&secret_key=YOUR_KEY"
-    my_md5 = hashlib.md5()  # get a MD5 encryption object
-    my_md5.update(str+secret_key)  # get the message's MD5 digest
-    my_md5_Digest = my_md5.hexdigest()  # return 32 bits digest in HEX
-    return my_md5_Digest.upper()
+class Md5tools:
+    
+    def get_sign_value(data_map):
+        data_maps = sorted(data_map.items(), key=lambda e: e[0], reverse=False)
+        str_data = urllib.urlencode(data_maps)
+        secret_key = "&secret_key=YOUR_KEY"
+        my_md5 = hashlib.md5()  # get a MD5 encryption object
+        my_md5.update(str_data + secret_key)  # get the message's MD5 digest
+        my_md5_digest = my_md5.hexdigest()  # return 32 bits digest in HEX
+        return my_md5_digest.upper()
 
 if __name__ == "__main__":
     dataMap = {"api_key": "YOUR_APIKEY", "secretkey": "YOUR_SECRETKEY"}
