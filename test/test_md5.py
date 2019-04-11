11c87168c9eb599616f3b97b29afabfb62d776ae
yanggf008 <yanggf008@126.com>
2019-04-11 15:46:49 +1200

    "Add unit test message"

--- test/test_md5.py
+++ test/test_md5.py
@@ -7,4 +7,5 @@ class TestMd5(unittest.TestCase):
     def test_md5_value(self):
         data_map = {"api_key": "YOUR_APIKEY", "secretkey": "YOUR_SECRETKEY"}
         md5_value_okex = Md5tools.get_sign_value(data_map)
-        self.assertEqual(md5_value_okex, "57A4E450D62D0FA5FB111D4413CDE925")
+        self.assertEqual(md5_value_okex, "57A4E450D62D0FA5FB111D4413CDE925",
+                         "md5 value should be equal")
