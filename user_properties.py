3bb636fdc8ad53f44ab12137b990a1430a7a6562
yanggf008@126.com <yanggf008@126.com>
2019-05-06 22:20:43 +1200

    delete file

--- user_properties.py
+++
@@ -1,8 +0,0 @@
-# coding: utf8
-"""
-    properties for user
-    APIKEY and SECRETKEY required
-"""
-APIKEY = "fbd46f3e-9013-4ba2-b5c5-d6179dd4526c"
-SECRETKEY = "FD96241DCA683BC2797B067A0423B19E"
-PASSPHRASE = "zhewanwang0526"
