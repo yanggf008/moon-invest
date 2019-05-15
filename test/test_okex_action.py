fa80342c84b04fa8c6ca104016091d0349881130
yanggf008@126.com <yanggf008@126.com>
2019-05-15 14:14:38 +1200

    "debug test_okex_action.py"

--- test/test_okex_action.py
+++ test/test_okex_action.py
@@ -6,7 +6,7 @@ from tools.okex_action import *
 class TestOkexAction(unittest.TestCase):
 
     def test_get_currencies(self):
-        c_list = get_currencies()
+        c_list = get_public_currencies()
         self.assertIsInstance(c_list, list, "The return value of get_currencies() should be a list")
         self.assertGreater(len(c_list), 1, "The number of currencies should be greater than 1")
 
