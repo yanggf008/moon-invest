6d611652a89ff8597abf760381171c3cd9c76f1f
yanggf008@126.com <yanggf008@126.com>
2019-05-17 10:09:36 +1200

    "add docstring in email_service.py"

--- tools/email_service.py
+++ tools/email_service.py
@@ -8,6 +8,13 @@ from email.mime.text import MIMEText
 class Email:
     @staticmethod
     def send(title, content):
+        """
+        This function is to send an email using the specified title and content
+        :param title: the title of the email
+        :param content: the content of the email
+        :return: None
+        :raise: Error: the email service is not stabilised
+        """
         server = smtplib.SMTP('smtp.126.com')
         server.login(SENDER_ADDRESS, str(base64.b64decode(EMAIL_PASSWORD), 'utf8'))
         message = MIMEText(content, 'html', 'utf-8')
