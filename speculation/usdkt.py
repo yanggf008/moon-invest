f5bfff2baccd99404df6bf9a2be9fd07111a4610
yanggf008@126.com <yanggf008@126.com>
2019-07-10 21:04:14 +1200

    "add speculation"

--- speculation/usdkt.py
+++ speculation/usdkt.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from tools.spot import *
+import time
 
 spot = Spot()
 
@@ -21,21 +22,21 @@ def buyKsellT_percent(kd_pair):
     return (can_sell_usdt - can_buy_usdk) / can_buy_usdk
 
 
-def trading_amount(kd_pair):
+def buyK_amount(kd_pair):
     instrument_usdk = kd_pair[0]
     instrument_usdt = kd_pair[1]
-    usdk_amount = spot.get_usdk()
-    usdt_amount = spot.get_usdt()
-    real_amount = 0.0
-    if is_one_percent(kd_pair) == "buyKsellT":
-        buy_cap = spot.get_kd_buy_price(instrument_usdk) * spot.get_kd_buy_amount(instrument_usdk)
-        sell_cap = spot.get_kd_sell_price(instrument_usdt) * spot.get_kd_sell_amount(instrument_usdt)
-        real_amount = min(min(buy_cap, sell_cap), usdk_amount)
-    if is_one_percent(kd_pair) == "buyTsellK":
-        buy_cap = spot.get_kd_buy_price(instrument_usdt) * spot.get_kd_buy_amount(instrument_usdt)
-        sell_cap = spot.get_kd_sell_price(instrument_usdk) * spot.get_kd_sell_amount(instrument_usdk)
-        real_amount = min(min(buy_cap, sell_cap), usdt_amount)
+    # usdk_amount = spot.get_usdk()
+    # usdt_amount = spot.get_usdt()
+    # real_amount = 0.0
+    buy_size = spot.get_kd_buy_amount(instrument_usdk)
+    sell_size = spot.get_kd_sell_amount(instrument_usdt)
+    real_amount = min(buy_size, sell_size)
     return real_amount
+    # if is_one_percent(kd_pair) == "buyTsellK":
+    #     buy_cap = spot.get_kd_buy_price(instrument_usdt) * spot.get_kd_buy_amount(instrument_usdt)
+    #     sell_cap = spot.get_kd_sell_price(instrument_usdk) * spot.get_kd_sell_amount(instrument_usdk)
+    #     real_amount = min(min(buy_cap, sell_cap), usdt_amount)
+    # return real_amount
 
 
 def trading_list():
@@ -56,11 +57,20 @@ def cmp(a, b):
 
 
 if __name__ == "__main__":
-    trading = trading_list()
-    sorted_list = sorted(trading, key=lambda x: x[3])
-    print(sorted_list)
-    instrument_id = sorted_list[-1][0]
-    print(instrument_id)
-    # spot.take_order(side="buy", instrument_id=instrument_id, price=buy_price, size=buy_size)
-
+    while True:
+        trading = trading_list()
+        loss_list = sorted(trading, key=lambda x: x[3])
+        profit_list = sorted(trading, key=lambda x: x[2])
+        instrument_id = loss_list[-1][0]
+        print(loss_list[-1])
+        print(profit_list[-1])
+        if loss_list[-1][3] > -0.011:
+            spot.take_order(otype="limit", side="buy", instrument_id=instrument_id,
+                            price=spot.get_kd_buy_price(instrument_id),
+                            size=buyK_amount((loss_list[-1][0], loss_list[-1][1])))
+            spot.take_order(otype="limit", side="sell", instrument_id=loss_list[-1][1],
+                            price=spot.get_kd_sell_price(loss_list[-1][1]),
+                            size=buyK_amount((loss_list[-1][0], loss_list[-1][1])))
+            time.sleep(5)
+        time.sleep(5)
 
