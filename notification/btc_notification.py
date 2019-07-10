338c1c87397efcad8ecbbd9d26943880a1346b63
yanggf008@126.com <yanggf008@126.com>
2019-07-10 21:13:23 +1200

    "add notification notify_exceed_price"

--- notification/btc_notification.py
+++ notification/btc_notification.py
@@ -1,4 +1,4 @@
-#coding: utf8
+# coding: utf8
 from tools.email_service import *
 from tools.spot import *
 from tools.kline_util import *
@@ -7,16 +7,24 @@ import time
 
 spot = Spot()
 
-def notify_fifteen_bottom():
-	processed_klines = get_processed_klines(10, "BTC_USDT", 900)
-	if is_bottom_pattern(processed_klines[-3], processed_klines[-2], processed_klines[-1]):
-		print("bottom pattern")
-		Email.send("15min", "bottom pattern")
 
-if __name__=="__main__":
-	Email.send("15min","it is a bottom pattern")
-	while True:
-		notify_fifteen_bottom()
-		time.sleep(5)
-	
-	
+def notify_fifteen_bottom():
+    processed_klines = get_processed_klines(10, "BTC_USDT", 900)
+    if is_bottom_pattern(processed_klines[-3], processed_klines[-2], processed_klines[-1]):
+        print("bottom pattern")
+        Email.send("15min", "bottom pattern")
+
+
+def notify_exceed_price(price):
+    current_price = spot.get_buy_price("BTC_USDT")
+    if current_price > float(price):
+        print("current price is higher than " + str(price))
+        Email.send("current price > " + str(price), "current price is " + str(current_price))
+
+
+if __name__ == "__main__":
+    Email.send("15min", "it is a bottom pattern")
+    while True:
+        notify_fifteen_bottom()
+        notify_exceed_price(13148.5)
+        time.sleep(5)
