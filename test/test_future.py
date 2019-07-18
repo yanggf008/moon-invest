4cbb9edcc7b5c6c60d3976cfc8edf0240001138f
yanggf008 <yanggf008@126.com>
2019-07-18 11:05:48 +0800

    change test future.py

--- test/test_future.py
+++ test/test_future.py
@@ -7,6 +7,6 @@ class TestFurture(unittest.TestCase):
     def test_get_account(self):
         future = FutureAPI()
         print(future.get_coin_account("BTC"))
-        print(future.get)
+	print(future)
         # TODO: test under normal wifi
         future.take_order(client_oid="guofengyang", instrument_id="BTC-USD-190719", otype="1", price="10430", size="0.183", match_price="1", leverage="10")
